476213ae0b0dc919e8ef1b97de0ce90422c08407
AB7zz <abhinavcv007@gmail.com>
2024-02-10 23:21:38 +0530

    init tailwind and contract

---
+++ contracts/contracts/counter.clar
@@ -0,0 +1,33 @@
+
+;; title: counter
+;; version:
+;; summary:
+;; description:
+
+;; traits
+;;
+
+;; token definitions
+;; 
+
+;; constants
+;;
+
+;; data vars
+;;
+
+;; data maps
+;;
+
+;; public functions
+
+(define )
+
+;;
+
+;; read only functions
+;;
+
+;; private functions
+;;
+
--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleCounter = () => {
+    setCount(count => count+1)
+  }
+
   return (
     <>
       <div>
@@ -16,18 +20,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Clarity-Stacks-Counter</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={handleCounter}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
